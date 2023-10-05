cf3d20cb9b22e7c509a086dd44b46b4c62458a8c
Cailean Finn <Caileannn@gmail.com>
2023-10-05 11:26:55 +0100

    Walker

--- Assets/New/Scripts/LockOrientation.cs
+++ Assets/New/Scripts/LockOrientation.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LockOrientation : MonoBehaviour
 {
+    public float y_offset = 0.0f;
+    public float z_offset = 0.0f;
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -14,7 +17,7 @@ public class LockOrientation : MonoBehaviour
     {
         Vector3 newRotation = new Vector3(0f, seat.rotation.eulerAngles.y, 0f);
         transform.rotation = Quaternion.Euler(newRotation);
-        Vector3 t_position = new Vector3(seat.position.x, 2.2f, seat.position.z);
+        Vector3 t_position = new Vector3(seat.position.x, y_offset, seat.position.z + z_offset);
         transform.position = t_position;
     }
 }
